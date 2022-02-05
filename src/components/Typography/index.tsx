fc27f6b14f0ab1ecc3793217d73e9d1be610f7fd
ordros <reordros@gmail.com>
2022-02-05 20:30:44 +0900

    add typography.

---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { ColorName, getColor } from '~/palette';
+
+type TextStyles = 'body' | 'body2' | 'header'; 
+
+type Props = {
+  children: React.ReactNode,
+  variant: TextStyles
+  bold?: boolean,
+  color?: ColorName,
+  className?: string,
+}
+
+const getFontSize = (variant: TextStyles) => {
+  switch(variant) {
+    case 'body':
+    case 'body2':
+    case 'header':
+      return '20px';
+  }
+};
+
+const StyledP = styled.p`
+  font-family: Work Sans;
+  font-style: normal;
+  font-weight: ${({ bold }) => bold ? 'bold' : 'normal'};
+  font-size: ${({ variant }) => getFontSize(variant)};
+  color: ${({ color }) => getColor(color)};
+  line-height: 23px;
+  display: flex;
+  align-items: center;
+  margin: 0;
+`;
+
+const Typography: React.FC<Props> = ({
+  children,
+  variant,
+  bold,
+  color = 'gray1',
+  className,
+}) => {
+  return <StyledP className={className} bold={bold} variant={variant} color={color}>{children}</StyledP>
+};
+
+export default Typography;
